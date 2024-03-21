2f80cb92334c95f4895de1685eddc26bc20854dd
Wilma Ljungkvist <wl222ek@student.lnu.se>
2024-03-21 14:54:42 +0100

    Add authService

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,10 +4,16 @@
  * @author Wilma Ljungkvist
  */
  import fetch from 'node-fetch'
+ import { AuthService } from '../services/authService.js'
  /**
   * Encapsulates a controller.
   */
  export class AuthController {
+
+  constructor() {
+    this.authService = new AuthService()
+  }
+
    /**
     * Renders a view and sends the rendered HTML string as an HTTP response.
     * index GET.
@@ -35,41 +41,14 @@
 
 
    async handleAuthorization (req, res, next) {
-    const Returnedcode = req.query.code
-
-    const params = new URLSearchParams();
-    params.append('client_id', process.env.CLIENT_ID);
-    params.append('client_secret', process.env.CLIENT_SECRET);
-    params.append('code', Returnedcode);
-    params.append('redirect_uri', process.env.REDIRECT_URI);
-    params.append('grant_type', 'authorization_code');
-
-    console.log(params)
-
-    const response = await fetch(process.env.GITLAB_TOKEN_URI, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded' // Change content type to application/x-www-form-urlencoded
-        },
-        body: params // Send parameters as URLSearchParams
-    })
-
-    if (!response.ok) {
-        throw new Error('Failed to exchange authorization code for access token');
-    }
-
-    const contentType = response.headers.get('content-type');
-    if (contentType && contentType.includes('application/json')) {
-        const tokenData = await response.json();
-        const accessToken = tokenData.access_token;
-        console.log('AccessToken' + accessToken)
-        res.redirect('/');
-    } else {
-        // Handle HTML response (error)
-        const htmlContent = await response.text();
-        console.error('Error response from OAuth provider:', htmlContent);
-        // You can choose to redirect to an error page or send an error message
-        res.status(500).send('Internal Server Error');
+    try {
+      const returnedcode = req.query.code
+      const tokenData = await this.authService.exchangeCodeForToken(returnedcode)
+      console.log(tokenData)
+      res.redirect('/auth')
+    } catch (error) {
+      console.error('Error occurred:', error)
+      res.status(500).send('Internal Server Error')
     }
 }
  
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,3 +1,31 @@
-class AuthService {
-    
+export class AuthService {
+    async exchangeCodeForToken(code) {
+        const params = new URLSearchParams()
+    params.append('client_id', process.env.CLIENT_ID)
+    params.append('client_secret', process.env.CLIENT_SECRET)
+    params.append('code', code);
+    params.append('redirect_uri', process.env.REDIRECT_URI)
+    params.append('grant_type', 'authorization_code')
+
+    const response = await fetch(process.env.GITLAB_TOKEN_URI, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: params
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to exchange authorization code for access token')
+    }
+
+    const contentType = response.headers.get('content-type')
+    if (contentType && contentType.includes('application/json')) {
+      return response.json()
+    } else {
+      const htmlContent = await response.text()
+      console.error('Error response from OAuth provider:', htmlContent)
+      throw new Error('Internal Server Error')
+    }
+    }
 }
